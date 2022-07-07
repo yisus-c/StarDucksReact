a9059e1f563aa38ccd2d3df954f7fbfbc449b1ef
yisus <guevarayisus1@gmail.com>
2022-07-07 17:27:24 -0500

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from "react"
 
 function App() {
+  const [clientes, setClientes] = useState([]);
+  const[empleados, setEmpleados] = useState([]);
+  const [items, setItems] = useState([]);
+  const [ordenes, setOrdenes] = useState([]);
+  
+  const addClientes = (cliente) => {
+    setClientes([...clientes, cliente])
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Cafeteria</h1>
       </header>
+
     </div>
   );
 }
---
+++ src/components/Interfaz.js
@@ -0,0 +1,8 @@
+import { useState } from "react";
+
+export const Interfaz = ({}) => {
+     
+    return (
+        console.log()
+    )
+}
